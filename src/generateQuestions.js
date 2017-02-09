90e3e3dbf063da84f35248abf58e7e5ec8c892d6
Sharp, Harry <Harry.Sharp@capitalone.com>
2017-02-09 17:39:52 +0000

    Fixed crashing error, ensured that second number is not 0 or 1

--- src/generateQuestions.js
+++ src/generateQuestions.js
@@ -1,16 +1,14 @@
 
 function generateQuestion() {
   const number1 = Math.floor((Math.random() * 100) + 1);
-  let number2 = Math.floor((Math.random() * number1));
+  let number2 = Math.floor((Math.random() * number1) + 2);
   const ops = ['+', '-', '*', '/'];
 
   const op = ops[Math.round(Math.random() * 3)];
 
-
-
   if (op === '/') {
-    while ((number1 % number2) != 0 || 1) {
-      number2 = Math.floor((Math.random() * number1 - 1) + 1);
+    while (number1 % number2 != 0) {
+      number2 = Math.floor((Math.random() * number1) + 2);
     }
   }
   return [number1, number2, op];
