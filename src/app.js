e5a16521c04b7dbda8c2e2103e9c36bf9cb22fe7
Sharp, Harry <Harry.Sharp@capitalone.com>
2017-02-23 14:39:48 +0000

    Updates to the html and css - making web page look prettier

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
 $(document).ready(() => {
   $(".generateButton").off('click').on('click', () => {
-    $("#correctness").text("");
+    $("#hCorrectness").text("");
+    $("#sCorrectness").text("");
     $("#Answer").val("");
     let question = generateQuestion();
-    $("#num1").text(question[0]);
-    $("#num2").text(question[1]);
-    $("#operator").text(question[2]);
+    $(".num1").text(question[0]);
+    $(".num2").text(question[1]);
+    $(".operator").text(question[2]);
     $(".puppyImage").remove();
   });
 
   $("#checkAnswer").off('click').on('click', () => {
     $(".puppyImage").remove();
-    const num1 = parseInt($("#num1").text());
-    const num2 = parseInt($("#num2").text());
-    const op = $("#operator").text();
+    const num1 = parseInt($(".num1").text());
+    const num2 = parseInt($(".num2").text());
+    const op = $(".operator").text();
     const answer = parseInt($("#Answer").val());
     if(checkAnswer(num1, num2, op, answer)) {
-      const correctness = $("#correctness");
-      correctness.text("Correct!");
-      $('<img class="puppyImage" alt="happy image" src="images/happyPuppy.jpg"/>').appendTo('.puppy');
+      $("#hCorrectness").text("Correct!");
+      $('<img class="puppyImage" alt="happy image" src="images/happyPuppy.jpg"/>').appendTo('.happyPuppy');
     } else {
-      $("#correctness").text("Incorrect, try again!");
-      $('<img class="puppyImage" alt="sad image" src="images/sadPuppy.jpg"/>').appendTo('.puppy');
+      $("#sCorrectness").text("Incorrect, try again!");
+      $('<img class="puppyImage" alt="sad image" src="images/sadPuppy.jpg"/>').appendTo('.sadPuppy');
     }
   });
 });
